b841e6d3f8ad8daa1e8ea71311770db30c935471
Alex Palmer <XanderXAJ@users.noreply.github.com>
2023-07-29 18:24:26 +0100

    Calculate hexadecimal offsets for RyzenAdj

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ from pathlib import Path
 # or add the `decky-loader/plugin` path to `python.analysis.extraPaths` in `.vscode/settings.json`
 import decky_plugin
 
+BASE_CPU = 0x100000
+BASE_GPU = 0x100000
+
 
 class Plugin:
     # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def update_offsets(self, cpu_offset, gpu_offset):
+    async def update_offsets(self, cpu_offset: int, gpu_offset: int):
         if cpu_offset > 0:
             cpu_offset = -cpu_offset
         if gpu_offset > 0:
             gpu_offset = -gpu_offset
 
+        cpu_value = hex(BASE_CPU + cpu_offset)
+        gpu_value = hex(BASE_GPU + gpu_offset)
         ra = Path(decky_plugin.DECKY_PLUGIN_DIR, "bin", "ryzenadj")
 
-        return f"Offsets updated: CPU: {cpu_offset}, GPU: {gpu_offset}, RyzenAdj found: {ra.exists()}"
+        return f"Offsets updated: CPU: {cpu_value} ({cpu_offset}), GPU: {gpu_value} ({gpu_offset}), RyzenAdj found: {ra.exists()}"
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
