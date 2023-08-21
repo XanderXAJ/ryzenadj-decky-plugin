9972d3dcfedf4901818c3d7e0208f68b06e99098
Alex Palmer <XanderXAJ@users.noreply.github.com>
2023-08-21 21:52:53 +0100

    Build plugin without root

--- .vscode/build.sh
+++ .vscode/build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 CLI_LOCATION="$(pwd)/cli"
 echo "Building plugin in $(pwd)"
-printf "Please input sudo password to proceed.\n"
 
-# read -s sudopass
-
-# printf "\n"
-
-echo $sudopass | sudo $CLI_LOCATION/decky plugin build $(pwd)
+# Rootless Docker usage requires a custom build of the CLI until a fix PR has been merged.
+# PR: https://github.com/SteamDeckHomebrew/cli/pull/6
+# Fixed code: https://github.com/XanderXAJ/decky-cli/tree/fix-rootless-bind-mount-permission
+$CLI_LOCATION/decky plugin build $(pwd)
