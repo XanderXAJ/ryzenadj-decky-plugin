2ac50ef0e2bfc9af5aca23abc81036afd86cc6f6
Alex Palmer <XanderXAJ@users.noreply.github.com>
2023-10-23 21:28:38 +0100

    Update PR state

--- .vscode/build.sh
+++ .vscode/build.sh
@@ -2,7 +2,7 @@
 CLI_LOCATION="$(pwd)/cli"
 echo "Building plugin in $(pwd)"
 
-# Rootless Docker usage requires a custom build of the CLI until a fix PR has been merged.
+# Rootless Docker usage requires a custom build of the CLI until the merged fix has been released in stable.
 # PR: https://github.com/SteamDeckHomebrew/cli/pull/6
 # Fixed code: https://github.com/XanderXAJ/decky-cli/tree/fix-rootless-bind-mount-permission
 $CLI_LOCATION/decky plugin build $(pwd)
