ce708c708f195da576bd77f2f940b894ab7e3d8b
Alex Palmer <XanderXAJ@users.noreply.github.com>
2023-10-23 21:29:42 +0100

    Output debug logging on Decky CLI builds

--- .vscode/build.sh
+++ .vscode/build.sh
@@ -5,4 +5,4 @@ echo "Building plugin in $(pwd)"
 # Rootless Docker usage requires a custom build of the CLI until the merged fix has been released in stable.
 # PR: https://github.com/SteamDeckHomebrew/cli/pull/6
 # Fixed code: https://github.com/XanderXAJ/decky-cli/tree/fix-rootless-bind-mount-permission
-$CLI_LOCATION/decky plugin build $(pwd)
+RUST_LOG=DEBUG $CLI_LOCATION/decky plugin build $(pwd)
